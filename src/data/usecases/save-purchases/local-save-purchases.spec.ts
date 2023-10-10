bf0d145e0631aa2992c427dbed9aa88604de87e2
wanderson almeida <wandersonalmeida.eng@gmail.com>
2023-10-10 14:11:01 -0300

    move production code

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,14 +1,7 @@
-class LocalSavePurchases{
-    constructor(private readonly cacheStore: CacheStore) {}
+import { CacheStore } from "@/data/protocols/cache"
+import { LocalSavePurchases } from "@/data/usecases/save-purchases/local-save-purchases"
 
-        async save(): Promise<void> {
-            this.cacheStore.delete('purchases')
-        }
-}
 
-interface CacheStore{
-    delete: (key: string) => void  
-}
 class CacheStoreSpy implements CacheStore{
     deleteCallsCount = 0
     key: string
